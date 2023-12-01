3ef8a3504ff705b6da33f910f72029024ff6d932
Bebra <trishechkin.sasha@yandex.ru>
2023-12-01 14:20:17 +0700

    Initial commit

---
+++ src/item.java
@@ -0,0 +1,51 @@
+public class item {
+    static int total_items_count;
+    String name;
+    float price;
+    int discount;
+    public item(String name, int price, int discount){
+        this.name = name;
+        this.discount = discount;
+        this.price = price;
+        if (!this.name.isEmpty()) {total_items_count++;}
+    }
+    public item(String name){
+        this.name = name;
+        if (!this.name.isEmpty()) {total_items_count++;}
+    }
+    public item(){
+        this.name = "";
+        this.discount = 0;
+        this.price = 0;
+    }
+    public void print() {
+        System.out.println("Name: " + name + ", Discount: " + discount + ", Price: " + price + ", Total items " + total_items_count);
+    }
+    public String get_name() {
+        return this.name;
+    }
+    public float get_price() {
+        return this.price;
+    }
+    public int get_discount() {
+        return this.discount;
+    }
+    public void set_name(String name) {
+        if (this.name.isEmpty() && !name.isEmpty()) {total_items_count++;}
+        this.name = name;
+    }
+    public void set_price(float price) {
+        this.price = price;
+    }
+    public void set_discount(int discount) {
+        this.discount = discount;
+    }
+    public static int get_total_items_count(){
+        return total_items_count;
+    }
+    public static void item_deleted(){
+        total_items_count--;
+    }
+}
+
+
---
+++ src/vspomog.java
@@ -0,0 +1,6 @@
+public class vspomog {
+    public static int get_int_price(item item_to_get){
+        int int_price = Math.round(item_to_get.get_price());
+        return int_price;
+    }
+}
